9e0adfd6f13902bc1540fb0a6560e81972c3edc1
Danny Nguyen <danny.danh.nguyen@gmail.com>
2021-05-06 18:04:47 -0700

    Added Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className='main-nav-section'>
+      <h1>Exoplanet Data Explorer</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
